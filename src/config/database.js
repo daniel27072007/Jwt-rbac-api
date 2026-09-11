f526a14fb1f4b8e10c313fde435a2da688575f8a
daniel27072007 <daniel.belculfine@gmail.com>
2026-09-11 14:49:19 -0300

    database.js - done

---
+++ src/config/database.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+const connectDatabase = async () => {
+    let MONGO_URI
+    if(process.env.NODE_ENV === 'test'){
+        MONGO_URI = process.env.MONGO_URI_DB_TEST
+    }
+    else{
+        MONGO_URI = process.env.MONGO_URI_DB
+    }
+    try {
+        mongoose.connect(MONGO_URI)
+        console.log('database connected')
+    } catch (error) {
+        console.error('Something went wrong when connecting to the databasse: ', error)
+        process.exit(1)
+    }
+}
+
+export default connectDatabase

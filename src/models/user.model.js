9183e7010edea8a4b3371d55c2309f4fef151a5f
daniel27072007 <daniel.belculfine@gmail.com>
2026-09-11 16:51:35 -0300

    fix user.model.js - roles -> role

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -4,7 +4,7 @@ const userSchema = new mongoose.Schema({
     name: {type: String, required: true, unique: true},
     email: {type: String, required: true, unique: true, lowercase: true},
     password: {type: String, required: true},
-    roles: {type: String, required: true, enum: ['Admin', 'Employee', 'Customer'], default: 'Customer'}
+    role: {type: String, required: true, enum: ['Admin', 'Employee', 'Customer'], default: 'Customer'}
 },{
     timestamps: true
 })

bfd21d487ac5b1bb9371c8a4dab910da86d20742
daniel27072007 <daniel.belculfine@gmail.com>
2026-09-13 21:25:56 -0300

    final project done

--- src/controllers/protectedController.js
+++ src/controllers/protectedController.js
@@ -1,14 +1,14 @@
 export const customerRoute = async (req, res) => {
     console.log('this is a route that customers have access');
-    res.json({ message: 'Hello Customer'})
+    res.status(200).json({ message: 'Hello Customer'})
 }
 
 export const employeeRoute = async (req, res) => {
     console.log('this is a route that employees have access');
-    res.json({ message: 'Hello Employee'})
+    res.status(200).json({ message: 'Hello Employee'})
 }
 
 export const adminRoute = async (req, res) => {
     console.log('this is a route that admins have access');
-    res.json({ message: 'Hello Admin'})
+    res.status(200).json({ message: 'Hello Admin'})
 }
